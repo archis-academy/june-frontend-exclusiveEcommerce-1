894aa56c2fdb5c8ff9b844ade430a7a8f77841ad
Meth <122786519+MethRAa@users.noreply.github.com>
2024-06-07 21:48:44 +0300

    API integration

---
+++ app.js
@@ -0,0 +1,11 @@
+async function getProducts() {
+  const productsResponse = await fetch("https://fakestoreapi.com/products");
+  const productsData = await productsResponse.json();
+  return productsData;
+}
+async function productsRender() {
+  // gelen datayı burda düzenlenmesi bekleyerek products içine atıyoruz ve bu fonksiyonun içinde kullanmaya devam ediyoruz
+  const products = await getProducts();
+}
+
+productsRender();
