f05e08820ece69872286284a236eb66f5067310a
Buse Ayrancıoğlu <ayrancioglu22@itu.edu.tr>
2024-06-16 23:29:48 +0300

    [JU-5]-Adding app.js file for timer

--- app.js
+++ app.js
@@ -9,3 +9,32 @@ async function productsRender() {
 }
 
 productsRender();
+
+function startCountdown(duration) {
+  let start = Date.now();
+
+  function timer () {
+    let diff = duration - (((Date.now() - start) / 1000) | 0);
+    let days = (diff / (60 * 60 * 24)) | 0; 
+    let hours = ((diff % (60 * 60 * 24)) / (60 * 60)) | 0;
+    let minutes = ((diff % (60 * 60)) / 60) | 0;
+    let seconds = (diff % 60) | 0;
+
+    document.getElementById("hoursValue").textContent = hours;
+    document.getElementById("daysValue").textContent = days;
+    document.getElementById("minutesValue").textContent = minutes;
+    document.getElementById("secondsValue").textContent = seconds;
+
+    if (diff <= 0) {
+      clearInterval(interval);
+    }
+  };
+
+  timer();
+
+  const interval = setInterval(timer, 1000);
+}
+
+window.onload = function () {
+  startCountdown(60 * 60 * 24);
+};
