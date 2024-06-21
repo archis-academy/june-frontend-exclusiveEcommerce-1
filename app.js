4c61df2fc46cd8a45976336727b8ef60df05b43c
unknown <abdullahs68400@gmail.com>
2024-06-21 11:34:33 +0300

    [JU-2] - adding app.js index and css editing

--- app.js
+++ app.js
@@ -9,3 +9,28 @@ async function productsRender() {
 }
 
 productsRender();
+
+function startCountdown() {
+  const targetDate = new Date();
+  targetDate.setDate(targetDate.getDate() + 4);
+
+  setInterval(function () {
+    const currentDate = new Date();
+    const difference = targetDate - currentDate;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    document.getElementById("todays-countdown-days").innerHTML = days + " :";
+    document.getElementById("todays-countdown-hours").innerHTML = hours + " :";
+    document.getElementById("todays-countdown-minutes").innerHTML =
+      minutes + " :";
+    document.getElementById("todays-countdown-seconds").innerHTML = seconds;
+  });
+}
+
+startCountdown();
