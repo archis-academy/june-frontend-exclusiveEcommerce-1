0819489f55b71acbb6a17d5f1d402d4012714430
Tahir-Mehrekule <nace4121@gmail.com>
2024-07-02 12:43:44 +0300

    edited js css

--- app.js
+++ app.js
@@ -4,9 +4,42 @@ async function getProducts() {
   return productsData;
 }
 
-async function productsRender() {
-  // Gelen datayı burada düzenlenmesi bekleyerek products içine atıyoruz ve bu fonksiyonun içinde kullanmaya devam ediyoruz
+const preProductsBtn = document.querySelector(".pre-button");
+const nextProductsBtn = document.querySelector(".next-button");
+const viewAllProductsBtn = document.querySelector(".btn-view-prd");
+
+let start = 0;
+let end = 8;
+
+nextProductsBtn.addEventListener("click", () => {
+  if (end <= 20) {
+    start += 8;
+    end += 8;
+    productsRender(start, end);
+  }
+});
+
+preProductsBtn.addEventListener("click", () => {
+  if (start > 0) {
+    start -= 8;
+    end -= 8;
+    productsRender(start, end);
+  }
+});
+
+viewAllProductsBtn.addEventListener("click", () => {
+  if (viewAllProductsBtn.textContent === "View All Products") {
+    viewAllProductsBtn.textContent = "View Less Products";
+    productsRender(0, 20);
+  } else {
+    productsRender(0, 8);
+    viewAllProductsBtn.textContent = "View All Products";
+  }
+});
+
+async function productsRender(start, end) {
   const products = await getProducts();
+  let slicedProducts = products.slice(start, end);
 
   function truncateTitle(title, maxLength) {
     return title.length > maxLength ? title.substring(0, maxLength) + "..." : title;
@@ -21,18 +54,26 @@ async function productsRender() {
     return fullStar.repeat(fullStarsCount) + emptyStar.repeat(emptyStarsCount);
   }
 
-  // Products array'ini burada oluşturuyoruz
-  document.querySelector("#exploreproducts").innerHTML = products.map((product) => {
-    return `
-    <div class="exploreprocontainer">
-      <div class="exproducts"><img class="exproimg" src=${product.image} alt="Product Image" ></div>
-      <div class="exprotitle"><h3>${truncateTitle(product.title, 25)}</h3></div>
-      <div class="expdattotaly">
-      <div class="exproprice"><p>$${product.price}</p></div>
-      <div class="exprorate"><p>${getStarRating(product.rating.rate)}</p></div>
-      <div class="exprocount"><p>(${product.rating.count})</p></div>
-       </div>
-    </div>`;
-  }).join("");
+  document.querySelector("#exploreproducts").innerHTML = slicedProducts
+    .map((product) => {
+      return `
+        <div class="exploreprocontainer">
+          <div class="exproducts">
+            <img class="exproimg" src=${product.image} alt="Product Image">
+            <span class="icons">
+              <i class="fa-regular fa-heart"></i>
+              <i class="fa-solid fa-cart-shopping"></i>
+            </span>
+          </div>
+          <div class="exprotitle"><h3>${truncateTitle(product.title, 25)}</h3></div>
+          <div class="expdattotaly">
+            <div class="exproprice"><p>$${product.price}</p></div>
+            <div class="exprorate"><p>${getStarRating(product.rating.rate)}</p></div>
+            <div class="exprocount"><p>(${product.rating.count})</p></div>
+          </div>
+        </div>`;
+    })
+    .join("");
 }
- productsRender();
+
+productsRender(start, end);
