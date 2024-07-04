4b5adf27b3de01c1c702762537221fd9949479de
Silabalkan <silabalkan.09@gmail.com>
2024-07-05 00:14:13 +0300

    [JU-9-created register.html/css/js and updated login.js]

--- login.js
+++ login.js
@@ -1,32 +1,42 @@
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    const email = document.getElementById('login-text');
-    const password = document.getElementById('login-password');
-    const loginButton = document.getElementById('log-in');
-
-    email.addEventListener("input", controlInput);
-    password.addEventListener("input",controlInput);
-
-    // email veya şifre kısmı boş mu kontrol
-    function controlInput(){
-        if(email.value.trim()==" " || password.value.trim()==" " ){
-            loginButton.disabled=true;
-            
-        }
-    }
-
-
     const form=document.getElementById("login-form");
+
     form.addEventListener("submit",(e)=>{
+
     e.preventDefault(); //formun gönderilmesini engeller
 
-    if(email.value.includes('@gmail.com') || email.value.includes('@hotmail.com')){
-          alert("Giriş Başarılı");
-    }else{
-        alert("Lütfen geçerli bir e-posta adresi giriniz!");
+    const email = document.getElementById('login-text').value.trim();
+    const password = document.getElementById('login-password').value.trim();
+    const loginButton = document.getElementById('log-in');
+
+     // email veya şifre kısmı boş mu kontrol eder
+     if(email===" " || password===" " ){
+        loginButton.disabled=true;   
     }
 
-});
+    if(!email.includes('@') || !email.includes('.com')){
+       alert("Lütfen doğru formatta e-posta adresi giriniz !");
+       return;
+  }
+
+    const userList = JSON.parse(localStorage.getItem('userList')) || [];
+
+    //hesabın kayıtlı olup olmadığını kontrol eder
+    const userIndex = userList.findIndex(user => user.mail === email && user.password === password);
+        if (userIndex === -1) {
+            alert("Hesap bulunamadı. Lütfen kayıt olunuz !");
+            
+        }else{
+            userList[userIndex].isLogin = true;
+            localStorage.setItem("userList", JSON.stringify(userList));
+            alert("Giriş Başarılı!");
+        }
 
 });
+
+
+
+
+
+
+  
