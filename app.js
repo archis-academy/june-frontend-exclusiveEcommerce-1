9934a6263f12343a9a2bce0ca4dd936fa9008975
FurkanOzpinar <frknzpnr@gmail.com>
2024-07-05 17:48:30 +0300

    [JU-4] - Fixed html & css files according to design, added some js

--- app.js
+++ app.js
@@ -11,12 +11,45 @@ async function productsRender() {
 productsRender();
 
 // Best Selling Products Section Start
-const likeEffect = document.getElementsByClassName("fa-heart");
+const bestSellingProductsContainer = document.getElementById("best-selling-products-api");
 
-likeEffect.addEventListener("click", () => {
-  if (likeEffect.style.color === "darkred") {
-    likeEffect.style.color = "default";
-  } else { 
-    likeEffect.style.color = "darkred";
+let bestSellingProducts = [];
+
+async function productsRender() {
+  const products = await getProducts();
+
+  for (let i = 0; i < products.length; i++) {
+    bestSellingProducts[i] = products[i];
   }
-});
+
+  bestSellingProducts.map((bsproducts) => {
+    return `<div class="homepage-best-selling-products-container-goods">
+              <div class="homepage-best-selling-products-container-goods-img">
+                <img src="${product.image}" alt="The North Coat">
+                <div class="icon-heart">
+                  <i class="fa-regular fa-heart fa-sm"></i>
+                  <span class="tooltip">Add to favourite</span>
+                </div>
+                <div class="icon-eye">
+                  <i class="fa-regular fa-eye fa-sm"></i>
+                  <span class="tooltip">Add to wishlist</span>
+                </div>
+              </div>
+              <div class="goods-info">
+                <h3>${product.title}</h3>
+                <div class="goods-price">
+                  <h3>$260</h3>
+                  <h3 class="base-price">$360</h3>
+                </div>
+                <div class="goods-rating">
+                  <i class="fa-solid fa-star fa-xs" style="color: #FFD43B;"></i>
+                  <i class="fa-solid fa-star fa-xs" style="color: #FFD43B;"></i>
+                  <i class="fa-solid fa-star fa-xs" style="color: #FFD43B;"></i>
+                  <i class="fa-solid fa-star fa-xs" style="color: #FFD43B;"></i>
+                  <i class="fa-solid fa-star fa-xs" style="color: #FFD43B;"></i>
+                  <h4 class="goods-amount">(65)</h4>
+                </div>
+              </div>
+            </div>`
+  })
+}
