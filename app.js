c9b8c9d08eabd7e001337dad78c9b6ce5d57746c
Tahir-Mehrekule <nace4121@gmail.com>
2024-07-09 19:08:16 +0300

    icon edit and js code Corrections have been made

--- app.js
+++ app.js
@@ -3,18 +3,32 @@ async function getProducts() {
   const productsData = await productsResponse.json();
   return productsData;
 }
-  var exproheart = document.getElementById("exproheart");
-  function Likexprohearth(){
-    if(exproheart.classList.contains("far")) {
-      exproheart.classList.remove("far");
-      exproheart.classList.add("fas");
-    }
-    else {
-      exproheart.classList.remove("far");
-      exproheart.classList.add("fas");
-    };
-};
+function Likexprohearth(productId) {
+  const exproheart = document.getElementById(`exproheart-${productId}`);
+  if (exproheart.classList.contains("far")) {
+    exproheart.classList.remove("far");
+    exproheart.classList.add("fas");
+    exproheart.style.color = "red"
+  }
+  else {
+    exproheart.classList.remove("fas");
+    exproheart.classList.add("far");
+    exproheart.style.color = ""
+  }
+}
 
+const ChangeIconExproShop = function(expshop) {
+  expshop.classList.toggle('fa-check')
+  if
+    (expshop.classList.contains("fa-check")){
+      expshop.style.color = "blue"
+      
+  }
+  else {
+    expshop.classList.remove("fa-check");
+    expshop.style.color = ""
+  }
+}
 
 const preProductsBtn = document.querySelector(".pre-button");
 const nextProductsBtn = document.querySelector(".next-button");
@@ -23,29 +37,42 @@ const viewAllProductsBtn = document.querySelector(".btn-view-prd");
 let start = 0;
 let end = 8;
 
-nextProductsBtn.addEventListener("click", () => {
-  if (end <= 20) {
-    start += 1;
-    end += 1;
-    productsRender(start, end);
-  }
-});
-
 preProductsBtn.addEventListener("click", () => {
   if (start > 0) {
     start -= 1;
     end -= 1;
     productsRender(start, end);
+    nextProductsBtn.disabled = false;
+  }
+  if (start === 0) {
+    preProductsBtn.disabled = true;
   }
 });
+
+nextProductsBtn.addEventListener("click", () => {
+  if (end < 20) {
+    start += 1;
+    end += 1;
+    productsRender(start, end);
+    preProductsBtn.disabled = false;
+  }
+  if (end === 20) {
+    nextProductsBtn.disabled = true;
+  }
+});
+
 
 viewAllProductsBtn.addEventListener("click", () => {
   if (viewAllProductsBtn.textContent === "View All Products") {
     viewAllProductsBtn.textContent = "View Less Products";
     productsRender(0, 20);
+    preProductsBtn.disabled = true;
+    nextProductsBtn.disabled = true;
   } else {
     productsRender(0, 8);
     viewAllProductsBtn.textContent = "View All Products";
+    preProductsBtn.disabled = false;
+    nextProductsBtn.disabled = false;
   }
 });
 
@@ -73,15 +100,15 @@ async function productsRender(start, end) {
           <div class="exproducts">
             <img class="exproimg" src=${product.image} alt="Product Image">
             <span class="exp-pro-icons">
-              <i onclick="Likexprohearth()" id=exproheart class="bla fa-regular fa-heart"></i>
-              <i class="fa-solid fa-cart-shopping"></i>
+              <div class="likeproduct"><i onclick="Likexprohearth(${product.id})" id="exproheart-${product.id}" class="far bla fa-regular fa-heart"></i></div>
+              <div class="exproshoping"><i onclick="ChangeIconExproShop(this)" id="exproshop-${product.id}" class="fas bla fa-solid fa-cart-shopping"></i></div>
             </span>
           </div>
           <div class="exprotitle"><h3>${truncateTitle(product.title, 20)}</h3></div>
           <div class="expdattotaly">
-            <div class="exproprice"><p>$${product.price}</p></div>
-            <div class="exprorate"><p>${getStarRating(product.rating.rate)}</p></div>
-            <div class="exprocount"><p>(${product.rating.count})</p></div>
+            <div class="exproprice"><span>$${product.price}</span></div>
+            <div class="exprorate"><span>${getStarRating(product.rating.rate)}</span></div>
+            <div class="exprocount"><span>(${product.rating.count})</span></div>
           </div>
         </div>`;
     })
