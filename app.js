b5cc6336ad545e799c3f0ab92e86575eb2c17a39
unknown <abdullahs68400@gmail.com>
2024-07-18 20:05:57 +0300

    [JU-] wishlist added

--- app.js
+++ app.js
@@ -54,6 +54,12 @@ async function productsRender(start, end) {
   selectedProducts.forEach((product) => {
     const productsCard = `
       <div class="products-kart">
+      <div onclick = "toggleWhislist(${product.id})" >
+      <svg  width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="17" cy="17" r="17" fill="white"/>
+<path d="M13 10C10.7912 10 9 11.7396 9 13.8859C9 15.6185 9.7 19.7305 16.5904 23.8873C16.7138 23.961 16.8555 24 17 24C17.1445 24 17.2862 23.961 17.4096 23.8873C24.3 19.7305 25 15.6185 25 13.8859C25 11.7396 23.2088 10 21 10C18.7912 10 17 12.3551 17 12.3551C17 12.3551 15.2088 10 13 10Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+      </div>
       <div class="img-box" onmouseenter="showAddToCart(this)" onmouseleave="hideAddToCart(this)">
         <img class="products-images" src="${product.image}" alt="${
       product.title
@@ -118,9 +124,29 @@ function hideAddToCart(hidden) {
   hidden.querySelector(".add-to-cart").style.display = "none";
 }
 
+async function toggleWhislist(productId) {
+  const wishlistProducts =
+    JSON.parse(localStorage.getItem("wishlistProducts")) || [];
+  if (wishlistProducts && !wishlistProducts.includes(productId)) {
+    localStorage.setItem(
+      "wishlistProducts",
+      JSON.stringify([...wishlistProducts, productId])
+    );
+  } else {
+    const newWishlistProducts = wishlistProducts.filter(
+      (productWishId) => productWishId !== productId
+    );
+    console.log(wishlistProducts);
+    localStorage.setItem(
+      "wishlistProducts",
+      JSON.stringify(newWishlistProducts)
+    );
+  }
+}
+
 async function addCartProduct(productId) {
   const products = await getProducts();
-  const cartProducts = JSON.parse(localStorage.getItem("cartProducts"));
+  const cartProducts = JSON.parse(localStorage.getItem("cartProducts")) || [];
   const product = products.find((product) => product.id == productId);
   const isProductInCart = cartProducts.some(
     (product) => product.id == productId
